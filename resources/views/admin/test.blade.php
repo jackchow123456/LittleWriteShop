d3a0e43041df711f33607020846b91247812e9fd
Jack <775893055@qq.com>
2020-06-01 14:05:50 +0800

    swoole

---
+++ resources/views/admin/test.blade.php
@@ -0,0 +1,39 @@
+<style>
+    .rpw {
+        display: inline-block;
+        margin: 0 10px;
+    }
+</style>
+<div class="col-md-8 goodsSelector">
+    <div class="rpw">
+        <img src="http://www.shop.com/upload/admin/images/v2-003115c453177ebcd367ff7ac687fbb1_hd.jpg"
+             style="width:100px;heigth:100px;"
+        ><br><span>测试商品 无规格</span><br><span>100</span>
+    </div>
+    <div class="rpw">
+        <img src="http://www.shop.com/upload/admin/images/bd7561ca05105d199a4c52ed7dd5b0da.jpg"
+             style="width:100px;heigth:100px;">
+        <br><span>测试商品（有规格）</span>
+        <br><span>100</span>
+        <input type="hidden" name="type_value" value="4,3">
+    </div>
+</div>
+
+<div class="form-group row form-field">
+    <div for="form-field-type-RXQLZ" class="col-md-2  text-capitalize control-label"><span>选择内容</span></div>
+    <div class="col-md-8">
+        <div class="rpw" style="display: inline-block;margin: 0 10px;"><span>衬衫</span></div>
+    </div>
+</div>
+
+<div class="form-group row form-field">
+    <div for="form-field-name-dX2vD" class="col-md-2  text-capitalize control-label">
+        <span>选择内容</span>
+    </div>
+    <div class="col-md-8">
+        <div class="input-group">
+            <span class="input-group-prepend"><span class="input-group-text bg-white"><i class="feather icon-edit-2"></i></span></span>
+            <input type="text" id="form-field-name-dX2vD" value="衬衫" disabled class="form-control name" placeholder="输入 名称">
+        </div>
+    </div>
+</div>
